c9830e268ad724acf44d6e499e1d69e63bea3f66
SantiBass <sibarraryan@gmail.com>
2021-11-28 14:54:22 -0800

    work-on-licence-link-function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,24 +2,30 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 // validte
-  if(license === MIT){
+  if(license === "MIT"){
     "![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
 }else{
-  return ""
+  return "";
 }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license=== MIT){
-    return `You can find information about MIT opensource here: https://opensource.org/licenses/MIT`
+  if (license=== "MIT"){
+    return `You can find information about MIT opensource here: https://opensource.org/licenses/MIT`;
+  }else{
+    return "";
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === "MIT"){
+    
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
