5143647cf4753c954fc013071d2df9dde717ccb5
SantiBass <sibarraryan@gmail.com>
2021-11-28 13:15:08 -0800

    first-commit

---
+++ index.js
@@ -0,0 +1,89 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Choices = require('inquirer/lib/objects/choices');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message : "What is your GitHub user name? ",
+        name : 'userName'
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'list',
+        message: 'What is your preferred method of communication?',
+        name: 'contact',
+        choices: ['email', 'phone', 'telekinesis']
+    },
+    {
+        type: 'input',
+        message: "What is your project's name?",
+        name : 'title',
+      // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'input',
+        message :'Write a short description of you project',
+        name : 'userName',
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'list',
+        message : "What kind of license your project have?",
+        name : 'license',
+        choices: ["No license", "Mit license"] 
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'input',
+        message : "What command should  be run to install dependencies? ",
+        name : 'installation'
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'input',
+        message : "What command should be run to run test? ",
+        name : 'runTest'
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'input',
+        message : "What does the user need to know about using the repo? ",
+        name : 'repoUse'
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+    {
+        type: 'input',
+        message : "What does the user need to know about contributing to the repo? ",
+        name : 'contribution'
+        // validate: (value)=> {if(value){return true}else {return 'you need a value to cntinue'}}
+
+    },
+
+];
+inquirer.prompt(questions)
+.then(data => {
+const fileName = `${data.userName.toLowerCase().split(" ").join("")}.js`
+
+})
+
+// console.log(questions)
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    //  fs.writeFile(`./${fileName}`)
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
